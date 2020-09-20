07b983c58490a4ed7660d341437abe3360d40794
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-09-20 12:28:36 +0200

    fix the menunav

--- src/script/main.js
+++ src/script/main.js
@@ -2,12 +2,26 @@ const nav = document.getElementById('nav');
 const menu = document.getElementById('menuBurger');
 const close = document.getElementById('close');
 
-console.log(menu)
-console.log(nav)
+const $leftArrow = document.getElementById('leftArrow');
+const $rightArrow = document.getElementById('rightArrow');
+
+const $headContent = document.querySelectorAll('.head__content')
+console.log($headContent)
+
+$rightArrow.addEventListener('click', () => {
+  $headContent.forEach(element => {
+    return element.classList.add('up')
+  });
+})
+
+$leftArrow.addEventListener('click', () => {
+  $headContent.forEach(element => {
+    return element.classList.remove('up')
+  });
+})
 
 menu.addEventListener('click', () =>{
   nav.classList.add('nav__isActive')
-  console.log('open')
 })
 
 close.addEventListener('click', () => {
