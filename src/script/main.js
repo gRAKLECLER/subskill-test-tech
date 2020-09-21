5190457d4c7a4d6d7aa35f0b3f4bd519c24e767f
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-09-21 17:14:34 +0200

    fixed responsive head section

--- src/script/main.js
+++ src/script/main.js
@@ -5,22 +5,6 @@ const close = document.getElementById('close');
 const $leftArrow = document.getElementById('leftArrow');
 const $rightArrow = document.getElementById('rightArrow');
 
-const $headContent = document.querySelectorAll('.head__content')
-console.log($headContent)
-
-console.log(window.screen.width)
-
-$rightArrow.addEventListener('click', () => {
-  $headContent.forEach(element => {
-    return element.classList.add('up')
-  });
-})
-
-$leftArrow.addEventListener('click', () => {
-  $headContent.forEach(element => {
-    return element.classList.remove('up')
-  });
-})
 
 menu.addEventListener('click', () =>{
   nav.classList.add('nav__isActive')
